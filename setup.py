388477f6bdcdfbe2a231f19f63909758ae443ce5
)a((o <j.c.vanelst@uu.nl>
2017-07-11 14:48:50 +0200

    ooops, setup.py

---
+++ setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup
+
+setup(name='labsync',
+      version='0.23prebug',
+      description='Lab data synchronisation package for Utrecht University',
+      classifiers=[
+        'Development Status :: 0.1 - RC1',
+        'License :: OSI Approved :: UNLICENSE License',
+        'Programming Language :: Python :: 3.5',
+        'Topic :: Data synchronisation :: WebDav :: Grid based Storage',
+      ],
+      keywords='Data synchronisation, WebDav, Grid based Storage',
+      url='https://github.com/UtrechtUniversity/Labdata_cleanup',
+      author='Jacco van Elst & Julia Brehm',
+      author_email='j.c.vanelst@uu.nl',
+      license='UNLICENSE',
+      packages=['labsync'],
+      include_package_data=True,
+      zip_safe=False)
